554b0f2395ff88bf92cd4888f16a704313cf5a26
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-07-19 12:23:25 +0300

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,25 @@
+package com.sectumsempra
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    
+    routing {
+        get("/") {
+            call.respond("Hello, route /")
+        }
+
+        get("/hello") {
+            call.respond("Hello, route /hello")
+        }
+    }
+}
+
